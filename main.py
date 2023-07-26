65f02d5ba801eb13927a10de04594643ad434c44
Jonny <jonathan.d.norris@gmail.com>
2023-07-26 15:17:27 -0500

    Update2

--- main.py
+++ main.py
@@ -1,52 +1,78 @@
+# Required Libraries
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
+# Load Data
+stops_report = pd.read_csv("Stops_Report.csv")
+phases = pd.read_excel("/mnt/data/phases.xlsx")
+region_lookup = pd.read_excel("/mnt/data/region lookup.xlsx")
+invoices_report = pd.read_csv("/mnt/data/Invoices_Report.csv")
+survey_report = pd.read_csv("/mnt/data/No_Sale_Survey.csv")
 
-def get_current_phase(df_phases, current_date):
-    """Determine the current phase based on the current date using the phases DataFrame."""
-    current_phase_row = df_phases[df_phases['Date'] == current_date]
-    current_phase = current_phase_row['Phase'].values[0]
-    return current_phase
+# Date for Yesterday
+yesterday = datetime.now() - timedelta(days=1)
+expected_day_of_sale = yesterday.strftime('%-m/%d/%Y') # Format date as m/dd/yyyy
 
+# Phase and Day for Yesterday
+phase_info_yesterday = phases[phases['Date'] == pd.Timestamp('2023-07-25')]
+day_mapping = {'Sunday': 1, 'Monday': 2, 'Tuesday': 3, 'Wednesday': 4, 'Thursday': 5, 'Friday': 6, 'Saturday': 7}
+phase_mapping = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4}
+day_yesterday_num = day_mapping[phase_info_yesterday['Day Of Week'].values[0]]
+phase_yesterday_num = phase_mapping[phase_info_yesterday['Phase'].values[0]]
 
-def load_and_prune_data(filenames, df_phases):
-    """Load data from CSV files and prune based on the current phase."""
-    dataframes = []  # List to hold dataframes
+# Filter Stops Report
+filtered_report = stops_report[(stops_report['Phase'] == phase_yesterday_num) & (stops_report['Day Of Week'] == day_yesterday_num)]
 
-    # Get the current date
-    current_date = datetime.today().date()
+# Add Region column
+merged_report = pd.merge(filtered_report, region_lookup, on='Territory', how='left')
 
-    # Calculate the current phase
-    current_phase = get_current_phase(df_phases, current_date)
+# Add Expected Day of Sale column
+merged_report['Expected Day of Sale'] = expected_day_of_sale
 
-    for filename in filenames:
-        # Load the data from the CSV file
-        df = pd.read_csv(filename)
+# Add Invoice on Expected Day column
+invoices_report['Date'] = pd.to_datetime(invoices_report['Date'])
+merged_report['Expected Day of Sale'] = pd.to_datetime(merged_report['Expected Day of Sale'])
+merged_report = pd.merge(merged_report, invoices_report[['Customer ID', 'Date']],
+                         left_on=['Customer ID', 'Expected Day of Sale'],
+                         right_on=['Customer ID', 'Date'],
+                         how='left')
+merged_report.rename(columns={'Date': 'Invoice on Expected Day'}, inplace=True)
 
-        # Prune the DataFrame by retaining only the rows where the 'Phase' matches the current phase
-        df_pruned = df[df['Phase'] == current_phase]
+# Add Sale Made on Expected Day? column
+merged_report['Sale Made on Expected Day?'] = merged_report['Invoice on Expected Day'].apply(lambda x: 'No' if pd.isnull(x) else 'Yes')
 
-        # Append the pruned dataframe to the list
-        dataframes.append(df_pruned)
+# Add Net Sales for Expected Day column
+net_sales = invoices_report[invoices_report['Date'] == expected_day_of_sale].groupby('Customer ID')['Total Cases'].sum().reset_index()
+net_sales.rename(columns={'Total Cases': 'Net Sales for Expected Day'}, inplace=True)
+merged_report = pd.merge(merged_report, net_sales, on='Customer ID', how='left')
 
-    return dataframes
+# Add Positive Sale column
+merged_report['Positive Sale'] = merged_report['Net Sales for Expected Day'].apply(lambda x: 1 if x > 0 else (0 if x <= 0 else float('nan')))
 
+# Add Last Sale Date column
+last_sale_date = invoices_report[invoices_report['Total Cases'] > 0].groupby('Customer ID')['Date'].max().reset_index()
+last_sale_date.rename(columns={'Date': 'Last Sale Date'}, inplace=True)
+merged_report = pd.merge(merged_report, last_sale_date, on='Customer ID', how='left')
+merged_report['Last Sale Date'] = merged_report.apply(lambda row: 'No Sale Last 6 Days' if pd.isnull(row['Invoice on Expected Day']) else row['Last Sale Date'], axis=1)
 
-# Filenames of the CSV files
-filenames = ['filename1.csv', 'filename2.csv', 'filename3.csv', 'filename4.csv']
+# Add Last Sale Date Amount column
+last_sale_date_amount = invoices_report[invoices_report['Total Cases'] > 0].sort_values('Date').groupby('Customer ID').tail(1)[['Customer ID', 'Total Cases']]
+last_sale_date_amount.rename(columns={'Total Cases': 'Last Sale Date Amount'}, inplace=True)
+merged_report = pd.merge(merged_report, last_sale_date_amount, on='Customer ID', how='left')
 
-# Load the phases data from the Excel file
-df_phases = pd.read_excel('/phases.xlsx')
+# Add Sale Complete On Expected Day? column
+merged_report['Sale Complete On Expected Day?'] = merged_report.apply(lambda row: 'Completed' if row['Sale Made on Expected Day?'] == 'Yes' and row['Last Sale Date Amount'] > 0
+                                                                      else ('Service Completed In Last 6 Days' if row['Sale Made on Expected Day?'] == 'No' and row['Last Sale Date Amount'] > 0
+                                                                      else '6 Day Non Buy'), axis=1)
 
-# Load the Region Lookup Data
-df_region_lookup = pd.read_excel('/region lookup.xlsx')
+# Add Survey Results column
+survey_report['Date'] = pd.to_datetime(survey_report['Date'])
+merged_report = pd.merge(merged_report, survey_report[['Customer ID', 'Date', 'Please select a reason why no sale took place:']],
+                         left_on=['Customer ID', 'Expected Day of Sale'],
+                         right_on=['Customer ID', 'Date'],
+                         how='left')
+merged_report.rename(columns={'Please select a reason why no sale took place:': 'Survey Results'}, inplace=True)
+merged_report.drop(columns=['Date'], inplace=True)
 
-
-
-# Convert the 'Date' column from string to datetime
-df_phases['Date'] = pd.to_datetime(df_phases['Date'])
-
-# Load and prune the data
-dataframes = load_and_prune_data(filenames, df_phases)
-
-
+# Save the final dataframe to a CSV file
+merged_report.to_csv('/mnt/data/Final_Report_Part2.csv', index=False)
