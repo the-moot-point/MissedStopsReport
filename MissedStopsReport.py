1c8be75524ddbbd30e7235bda1796d8a8abd537b
Mooty <jonathan.d.norris@gmail.com>
2023-08-04 14:49:11 -0500

    Almost final version of this file.

--- MissedStopsReport.py
+++ MissedStopsReport.py
@@ -1,13 +1,16 @@
 # Required Libraries
 import pandas as pd
 from datetime import datetime, timedelta
+import os
 
 
 def update_survey_results(row):
-    if row['Sale Complete On Expected Day?'] in ['Completed', 'Service Completed In Last 6 Days']:
-        return 'Completed'
+    if pd.notnull(row['Survey Results']):
+        return row['Survey Results']
     elif row['Last Sale Date'] == 'No Sale Last 6 Days' and row['Sale Complete On Expected Day?'] == '6 Day Non Buy':
         return 'Missed Stop'
+    elif row['Sale Complete On Expected Day?'] in ['Completed', 'Service Completed In Last 6 Days']:
+        return 'Completed'
     else:
         return row['Survey Results']
 
@@ -15,17 +18,23 @@ def update_survey_results(row):
 def main():
     # File paths
     path_stops_report = "Stops_Report.csv"
-    path_phases = "/config/phases.xlsx"
-    path_region_lookup = "/config/region lookup.xlsx"
+    path_phases = "config/phases.xlsx"
+    path_region_lookup = "config/region lookup.xlsx"
     path_invoices_report = "Invoices_Report.csv"
     path_survey_report = "No_Sale_Survey.csv"
+    file_path = 'Stops_Worksheet.csv'
 
-    # Load Data
+    # Load Data and survey columns
     stops_report = pd.read_csv(path_stops_report)
     phases = pd.read_excel(path_phases)
     region_lookup = pd.read_excel(path_region_lookup)
     invoices_report = pd.read_csv(path_invoices_report)
+    invoices_report['Date'] = pd.to_datetime(invoices_report['Date'])
+    current_date = pd.to_datetime(datetime.now().date())
+    invoices_report = invoices_report[invoices_report['Date'] != current_date]
     survey_report = pd.read_csv(path_survey_report)
+    survey_report.rename(columns={'Date Completed': 'Date'}, inplace=True)
+    survey_report.rename(columns={'Customer Num': 'Customer ID'}, inplace=True)
 
     # Date for Yesterday
     yesterday = datetime.now() - timedelta(days=1)
@@ -63,18 +72,19 @@ def main():
 
     # Add Net Sales for Expected Day column
     net_sales = invoices_report[invoices_report['Date'] == yesterday].groupby(
-        'Customer ID')['Total Cases'].sum().reset_index()
+        'Customer ID')['Total Cases'].sum().round(2).reset_index()
     net_sales.rename(columns={'Total Cases': 'Net Sales for Expected Day'}, inplace=True)
     merged_report = pd.merge(merged_report, net_sales, on='Customer ID', how='left')
 
     # Add Positive Sale column
-    merged_report['Positive Sale'] = merged_report['Net Sales for Expected Day'].apply(lambda x: 1 if x != 0 else 0)
+    merged_report['Positive Sale'] = merged_report['Net Sales for Expected Day'].apply(lambda x: 1 if x != 0 and pd.notnull(x) else 0)
 
     # Add Last Sale Date column
     last_sale_date = invoices_report[invoices_report['Total Cases'] != 0].groupby('Customer ID')['Date'].max().reset_index()
     last_sale_date.rename(columns={'Date': 'Last Sale Date'}, inplace=True)
     merged_report = pd.merge(merged_report, last_sale_date, on='Customer ID', how='left')
-    merged_report['Last Sale Date'] = merged_report.apply(lambda row: 'No Sale Last 6 Days' if pd.isnull(row['Invoice on Expected Day']) else row['Last Sale Date'], axis=1)
+    merged_report['Last Sale Date'] = merged_report['Last Sale Date'].fillna('No Sale Last 6 Days')
+
 
     # Add Last Sale Date Amount column
     last_sale_date_amount = invoices_report[invoices_report['Total Cases'] != 0].sort_values('Date').groupby('Customer ID').tail(1)[['Customer ID', 'Total Cases']]
@@ -82,9 +92,13 @@ def main():
     merged_report = pd.merge(merged_report, last_sale_date_amount, on='Customer ID', how='left')
 
     # Add Sale Complete On Expected Day? column
-    merged_report['Sale Complete On Expected Day?'] = merged_report.apply(lambda row: 'Completed' if row['Sale Made on Expected Day?'] == 'Yes' and row['Last Sale Date Amount'] != 0
-                                                                      else ('Service Completed In Last 6 Days' if row['Sale Made on Expected Day?'] == 'No' and row['Last Sale Date Amount'] != 0
-                                                                      else '6 Day Non Buy'), axis=1)
+    merged_report['Sale Complete On Expected Day?'] = merged_report.apply(
+        lambda row: 'Completed' if row['Sale Made on Expected Day?'] == 'Yes' and row['Last Sale Date Amount'] != 0
+        else ('Service Completed In Last 6 Days' if row['Sale Made on Expected Day?'] == 'No' and pd.notnull(
+            row['Last Sale Date Amount']) and row['Last Sale Date Amount'] != 0
+              else (
+            '6 Day Non Buy' if row['Sale Made on Expected Day?'] == 'No' and pd.isnull(row['Last Sale Date Amount'])
+            else 'Unknown')), axis=1)
 
     # Add Survey Results column
     survey_report['Date'] = pd.to_datetime(survey_report['Date'])
@@ -98,8 +112,14 @@ def main():
     # Update Survey Results column based on the additional conditions
     merged_report['Survey Results'] = merged_report.apply(update_survey_results, axis=1)
 
-    # Save the final dataframe to a CSV file
-    merged_report.to_csv('Stops_Worksheet.csv', index=False)
+    # Drop Dupes and save the final dataframe to a CSV file
+    merged_report.drop_duplicates(subset='Customer ID', keep='first', inplace=True)
+    # Check if file exists
+    if os.path.isfile(file_path):
+        print(f"File {file_path} exists and will be overwritten.")
+
+    # Save the DataFrame to a CSV, overwriting any existing file
+    merged_report.to_csv(file_path, index=False)
 
 
 if __name__ == "__main__":
